0c485a34db81c00b935d24b01ddfa630aed7859f
Yixuan Chen <chenyixuan@iscas.ac.cn>
2025-10-10 18:08:27 +0800

    vx vlmax

--- scripts/VXLiteral.py
+++ scripts/VXLiteral.py
@@ -273,6 +273,7 @@ vx_literal_mask_body = '''
 
   auto sew = op->typeInfo->sew.to_int();
   P.VU.vsew = sew;
+  P.VU.vlmax = length;
   auto RS1 = *dataB; // for vrgather.vx
 
   for (int i = 0; i < length; ++i) {
